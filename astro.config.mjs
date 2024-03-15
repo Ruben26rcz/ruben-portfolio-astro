548e612cc5e7efc051ad801f569d0f478e84c425
Ruben26rcz <ruben26rcz@gmail.com>
2024-03-15 18:07:14 +0100

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import pageInsight from "astro-page-insight";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [pageInsight()]
+});
