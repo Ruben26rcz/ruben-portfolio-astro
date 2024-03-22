0b40eacef132b90dd0e7e79877a72acf6ee9d988
Ruben26rcz <ruben26rcz@gmail.com>
2024-03-22 14:16:57 +0100

    fix(page-insights): Redeploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config'
-
 import pageInsight from 'astro-page-insight'
 import vercel from '@astrojs/vercel/serverless'
 
-// https://astro.build/config
 export default defineConfig({
 	output: 'server',
 	adapter: vercel({
